f3f96520ff547f20a2d8dae98c264d2e7384095c
SillyProphet <austinjw206@gmail.com>
2026-02-06 18:33:14 -0600

    Image uploading and viewing are completed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const multer = require("multer");
 
 const API_KEY = process.env.API_KEY;
 const PORT = process.env.PORT;
 const pool = require('./dbpool');
+const upload = require('./upload');
 const bodyParser = require('body-parser');
 
 function APIKeyCheck(req, res, next) {
@@ -18,16 +18,7 @@ function APIKeyCheck(req, res, next) {
     }
 }
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  },
-});
-const upload = multer({ storage: storage });
-
+app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.json());
 app.use(APIKeyCheck);
 app.use(express.urlencoded({ extended: true }));
@@ -36,9 +27,20 @@ app.listen(PORT, () => {
     console.log('Server active on port ' + PORT);
 });
 
-app.post('/api/upload', upload.single('file'), async (req, res) => {
-    console.log(req.body.username);
-    res.send("uploaded!")
+app.post('/api/upload', upload.single('image'), async (req, res) => {
+    const Image = req.file.path;
+    const Username = req.body.username;
+    const Posts = req.body.post;
+
+    try {
+        await pool.query('insert into islands (username, post, imageurl) values (?, ?, ?)', 
+        [Username, Posts, Image]);
+        res.send('Upload Completed')
+    } 
+    catch (err) {
+        console.log(err);
+        res.status(500).send('Failed to upload Island!')
+    }
 })
 
 app.get('/api/ocean', async (req, res) => {
@@ -49,7 +51,8 @@ app.get('/api/ocean', async (req, res) => {
             const Page = req.query.page * 10 - 11 + 1 || 0;
             const [rows] = await pool.query('SELECT * FROM islands WHERE id > ' + Page + " LIMIT 10");
             res.json(rows);
-        } catch (err) {
+        } 
+        catch (err) {
             console.error(err);
             res.status(500).send('Failed to fetch islands');
         }
@@ -59,7 +62,8 @@ app.get('/api/ocean', async (req, res) => {
             const Page = (req.query.page - 1) * 10;
             const [rows] = await pool.query('SELECT * FROM islands ORDER BY id DESC LIMIT 10 OFFSET ' + Page);
             res.json(rows);
-        } catch (err) {
+        } 
+        catch (err) {
             console.error(err);
             res.status(500).send('Failed to fetch islands');
         }
